3e256e45a906fbc0a64f5d511ef9cd69e8c08692
Misha <purtov4295@gmail.com>
2019-05-17 12:24:05 +0700

    Add intersect

--- src/main.c
+++ src/main.c
@@ -4,24 +4,39 @@
 
 int main()
 {
-    int w0 = 1;
     FILE* file;
-    file = fopen("coordinates.txt", "r");
+    file = fopen("./input/c.txt", "r");
     if (!file) {
         printf("Can`t open file\n");
         return -1;
     }
-    char str[100];
-    Circle* c = (Circle*)malloc(sizeof(*c));
+    char str[MAX];
+    Circle* c = (Circle*)malloc(sizeof(Circle) * MAX);
+    int i = 0;
+    int count_err = 0;
+    int check_ret;
     while (!feof(file)) {
-        fgets(str, sizeof(str), file);
-        if (run(str, c) == -1) {
-            return -1;
+        fgets(str, MAX, file);
+        // if (strcmp(str, "\n\0") == 0) {
+        //     continue;
+        // }
+        i++;
+        (c + i)->index = i;
+        check_ret = scan_errors(c, str, i);
+        if (check_ret == 1 || check_ret == 2 || check_ret == 3 || check_ret == 4
+            || check_ret == 5) {
+            print_error(check_ret, i, count_err);
+            i--;
+            count_err++;
+            continue;
         }
-        print_circle(c, w0);
-        w0++;
     }
 
+    print_circle(c, i);
+
     fclose(file);
+    free(c);
+    c = NULL;
+
     return 0;
 }
